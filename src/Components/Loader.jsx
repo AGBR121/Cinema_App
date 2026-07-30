f45b8935de8da4717945f8e7f7f4cc17639fc234
AGBR121 <angel.burbano@utp.edu.co>
2026-07-30 17:23:45 -0500

    reanudar proyecto

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,12 @@
+import '../styles/Loader.css'
+
+function Loader() {
+  return (
+    <div className="loader-wrapper">
+      <div className="loader-spinner" />
+      <p className="loader-text">Cargando...</p>
+    </div>
+  )
+}
+
+export default Loader
